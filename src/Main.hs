55d8b5362175990c109f439b8ff89a16c967e44e
aj <aj@dev>
2015-04-21 16:32:09 -0400

    main now allows synctest

--- src/Main.hs
+++ src/Main.hs
@@ -1,52 +1,36 @@
-import Pipes
+import           Pipes
 --import qualified Pipes.Prelude as P
-import qualified Pipes.ByteString as PB 
+import qualified Pipes.ByteString           as PB
 --import qualified Pipes.Network.TCP as PTCP
 --import qualified Pipes.Lift as PL
 --import System.Process
 --import System.Cmd
-import System.IO
+import           System.IO
 --import Network.Socket
-import qualified Data.ByteString.Lazy as BSL
-import qualified Data.ByteString as BS
+import qualified Data.ByteString            as BS
+import qualified Data.ByteString.Lazy       as BSL
 --import GHC.Generics (Generic)
-import WavePacket
+import           WavePacket
 --import Server
-import Data.Time.Clock
-import Data.Time
---import Data.Time.Format
+import           Data.Time.Clock
+import Data.Time.Format
 import System.Locale
-import OpenALTest
-import qualified Control.Concurrent as C
+import Data.Maybe
+import qualified Control.Concurrent         as C
+import           OpenALTest
 --import Control.Concurrent.STM
 --import Control.Concurrent.STM.TVar
 --import Sound.OpenAL
-import Control.Monad.State.Strict as S
+import           Control.Monad.State.Strict as S
 --import Data.Attoparsec.ByteString.Lazy
-import Data.Binary
-import qualified Network.NTP.Control as NTP
-import qualified Network.NTP.Control.Packet as NTPP
-import Data.Time.Clock.POSIX
-import Data.Maybe
-
-getNTPTime :: String -> IO UTCTime
-getNTPTime hostname = do
-    t <- NTP.queryHost hostname $ NTPP.opVariables [NTPP.Clock]
-    v <- NTPP.readVariables t
-    return $ posixSecondsToUTCTime $ fromJust $ NTPP.clock v
-
-timeTest :: IO ()
-timeTest = do
-    t <- getNTPTime "localhost"
-    let f = "%F %X:%q"
-    let s = formatTime defaultTimeLocale f t
-    print $ encode s
-    print $ BSL.length $ encode s
+import           Data.Binary
+import System.Environment
 
 main :: IO ()
 main = do
-    t <- getCurrentTime
-    syncTest t "part1.wav" 
+  (timeString:file:_) <- getArgs
+  let t = fromJust $ parseTime defaultTimeLocale "%F %X" timeString
+  syncTest t file
 
 op :: Consumer PB.ByteString IO ()
 op = flip evalStateT (0::Int) $ forever $ do
@@ -66,7 +50,8 @@ syncTest t filename = do
     runEffect $ PB.fromHandle f >-> syncPipe t >-> alFormatPipe >-> alConsumer alSource
     now <- getCurrentTime
     print $ "Waiting " ++ (show $ (timeDiff t now) `div` 1000000) ++ " seconds"
-    C.threadDelay $ timeDiff t now
+    _ <- getLine
+    return ()
 
 pulseTest :: IO ()
 pulseTest = do
